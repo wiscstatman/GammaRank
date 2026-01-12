89041d59fba8a6364f1871a330ec6a3230c86538
manewton <wiscstatman@gmail.com>
2026-01-11 17:18:32 -0700

    first isotonic probability calculated

--- isoreg/01-iso.R
+++ isoreg/01-iso.R
@@ -16,3 +16,23 @@ fit$x <- dat$WINTER[ok]
 par( mar=c(3,3,3,0))
 plot(fit, main="Ice-on days, Lake Mendota", xlab="year", ylab="days", las=1 )
 legend( "bottomleft", legend="isotonic regression", lwd=1, col="red" )
+
+### recall Gamma/Poisson conjugacy
+### if lambda ~ Gamma[ alpha, beta ]; X|lambda ~ Poisson(lambda)
+## then lambda | x ~ Gamma[ alpha + x, beta + 1 ]
+
+
+## so shapes should be alpha + days
+## rates should be beta + 1
+
+## E(days) = E(lambda ) = alpha/beta   ### e.g. alpha = 200, beta = 2
+
+post.shape <- as.integer( y + 200 )
+post.rate <- rep(3, length(y))
+
+## too big...let's try a small one
+
+ok <- rep(FALSE, length(y))
+ok[1:20] <- TRUE
+
+iso.p <- grankp( post.shape[ok], post.rate[ok] )
